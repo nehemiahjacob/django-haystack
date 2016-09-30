5a6ba9ee76db24f8f300c796b86876fce45c6668
nehemiah <neo@madmantour-lm-2.local>
2016-09-30 10:11:21 +1000

    Bumping version number

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='django-haystack',
-    version='2.5.2',
+    version='2.5.3',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
